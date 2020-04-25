cc47420e3b8bead808c221cbdf6710053ed41e10
Stardusted1 <w73184@gmail.com>
2020-04-25 23:42:42 +0300

    Update Greeting.java

--- src/main/java/marko/andrushchenko/networklab1/controllers/Greeting.java
+++ src/main/java/marko/andrushchenko/networklab1/controllers/Greeting.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 
@@ -16,9 +17,10 @@ public class Greeting {
 	String mainPage(Model model){
 		return "greeting";
 	}
-	@GetMapping("/{name}")
-	String greeting(@PathVariable String name, Model model){
-		model.addAttribute(name);
-		return "greetingpage";
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 }
